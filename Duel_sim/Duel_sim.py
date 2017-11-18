445464870998167e801423d7d1f82bb2ec027830
Dissapointment-JP <yetizeebdeeb@gmail.com>
2017-11-18 11:31:51 -0500

    made code more consistent

--- Duel_sim/Duel_sim.py
+++ Duel_sim/Duel_sim.py
@@ -1,32 +1,27 @@
-print """
-    WELCOME TO 1V1
-    DUELING SIM
-    ############"""
+#!/usr/bin/env python
+
+print """   WELCOME TO 1V1\n    DUELING SIM\n   ############"""
 def help():
-    print """
-    These are the Commands you can execute
-    e- exit game
-    w- choose your weapon
-    e1-e5 choose your opponent
-    b- start the battle
-    ba- battle command menu"""
+    print """    These are the Commands you can execute\n    e,q- exit game\n    w- choose your weapon\n    e1-e5 choose your opponent\n \
+   b- start the battle\n    ba- battle command menu"""
 
 def spear():
     namewep1='spear'
-
+    print namewep1
 def staff():
     namewep2='staff'
-
+    print namewep2
 def mace():
     namewep3='mace'
+    print namewep3
 
 def enemy1():
-    print'ENEMY NAME\n Jack\n Difficulty\n Easy\n'
-    print'Has 5hp'
+    print 'ENEMY NAME\n Jack\n Difficulty\n Easy\n'
+    print 'Has 5hp'
     
 def enemy2():
-    print'ENEMYNAME\n Robert\n Difficulty\n Med\n'
-    print'HAS 7HP'
+    print 'ENEMYNAME\n Robert\n Difficulty\n Med\n'
+    print 'HAS 7HP'
 
 def enemy3():
     print'ENEMYNAME\n KEKBOI MCRARE PEPE Difficulty\n Hard'
@@ -42,9 +37,8 @@ def sword():
     print"""
     This is the sword 
     this is one of the weapons you can use in 
-    your battle scenarios
-    
-"""
+    your battle scenarios"""
+
 def ba():
     print"""
     This is the battle command menu
@@ -63,11 +57,31 @@ def b3():
     for i in range(10):
      battle_command1=raw_input('>>>')
      if battle_command1=='u':
-         print'you have done 3 dmg with' + spear()
-
+        print'you have done 3 dmg with' + spear()
+        print'Your enemy staggers and dies'
+    if battle_command=='st':
+        print'you have done 7 damage with' + spear()
+        print'your enemy shreiks and dies squirming on the floor'
+    if battle_command=='sl':
+        print'you have done 4 damage with' + spear()
+        print' your enemy grasps his side as he falls to the ground'
+                
 def kys():
     while True:
         print'Pls Consider suicide'
+def victory_credits():
+    print"""
+        YOU MADE IT 
+        GAME:
+        DUELING SIM 
+        MADE BY:
+        RYAN DALE 
+        FROM THE VSOSS TEAM 
+        THANK YOU FOR PLAYING"""
+    x=raw_input('are you sure you want to quit')
+    if x=='y' or 'yes':
+        exit()
+
 while True:
     command=raw_input('>>>')
 
@@ -75,21 +89,21 @@ while True:
      enemy1()
      b1()
 
-    if command == 'enemy2':
+    if command=='enemy2':
      enemy2()
      b2()
 
-    if command =='enenmy3':
+    if command=='enenmy3':
      enemy3()
      b3()
     
-    if command =='spear':
+    if command=='spear':
      spear()
     
-    if command =='mace':
+    if command=='mace':
      mace()
 
-    if command =='staff':
+    if command=='staff':
      staff()
 
 #if command=='e4':
@@ -100,21 +114,16 @@ while True:
    # b5()
 #will only do 4 and 5 if i have time lol
 
-    if command=='e':
-         quit()
-
+    if command=='e' or command=='q':
+         exit()
     if command=='w':
          w()
-
     if command=='ba':
         ba()
-
     if command=='help':
         help()
-    if command == 'sword':
+    if command=='sword':
         sword()
-    if command == 'boi'
-        pass
-    if command =='kys'
         kys()
-
+    if command=='kys' or command=='boi':
+        kys()
