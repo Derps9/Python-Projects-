63e48e5d223b3589c0ca13f0d51330aa11b24be8
Ryan <ryandale0324@gmail.com>
2017-12-02 15:09:26 -0500

    hey

---
+++ email_verif.py
@@ -0,0 +1,7 @@
+print """welcome to email verifier
+        this is version 1.0"""
+verif=raw_input('enter your email here')
+if verif== ['.com']:
+	print 'Your email is good'
+else:
+	print 'your email is not good'
