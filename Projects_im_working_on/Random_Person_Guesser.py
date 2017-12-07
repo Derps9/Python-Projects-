6a38a43f30844131c44c6ab3588ccfa818faad0a
Ryan <ryandale0324@gmail.com>
2017-12-06 20:48:11 -0500

    finshed school project

--- Projects_im_working_on/Random_Person_Guesser.py
+++ Projects_im_working_on/Random_Person_Guesser.py
@@ -6,35 +6,66 @@ def start():
 Type guess to get a random person from this class"""
 
 def guess():
-    person= random.randint(1,25)
+    person= random.randint(1,28)
     if person == 1:
-        print'Ryan'    
+        print'Faheem'    
     if person == 2:
-        print 'Jacob'
+        print 'Andrew'
     if person == 3:
-        print'lucas'
+        print'David'
     if person == 4:
-        print'aiden'
+        print'Ryan'
     if person == 5:
-        print'Bucari'
+        print'Bakarri'
     if person == 6:
-        print 'isiah'
+        print 'Alex'
     if person == 7:
-        print'Devin'
+        print'Jordyn'
     if person == 8:
-        print 'Zach'
+        print 'isiah'
     if person == 9:
-        print 'kayla'
+        print 'Ben'
     if person == 10:
-        print 'blake'
-    if person == 10:
-        pass
+        print 'Ana'
     if person == 11:
-        pass
+        print'Charity'
     if person == 12:
-        pass
+        print 'Gabriella'
     if person == 13:
-        pass
+        print 'Devin'
+    if person == 14:
+        print 'Zach'
+    if person == 15:
+        print 'Amy'
+    if person == 16:
+        print 'Sydney'
+    if person == 17:
+        print 'Austin'
+    if person == 18:
+        print 'Aiden'
+    if person == 19:
+        print ' Alyssa'
+    if person == 20:
+        print 'Ashlyn'
+    if person == 21:
+        print 'Lucas'
+    if person == 22:
+        print 'Jacob'
+    if person == 23:
+        print 'Christine'
+    if person == 24:
+        print 'Blake'
+    if person == 25:
+        print 'Kayla'
+    if person == 26:
+        print 'Jaden'
+    if person == 27:
+        print 'Hailey'
+    if person == 28:
+        print 'Brianna'
+    if person == 29:
+        print 'pagoaga'
+
 while True:
     command= raw_input('>>>')
     
