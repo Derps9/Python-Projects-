8f090b575d28da5348a9186b9a32e1f6539749ed
Ryan <ryandale0324@gmail.com>
2017-12-10 20:06:21 -0500

    lol

--- makeing_my_own_small_program.py
+++ makeing_my_own_small_program.py
@@ -3,8 +3,9 @@ class spinner:
     def __init__(self,name):
         self.name=name
 
-ryan = spinner('ryan')
-
+ryan = spinner('ryan,')
+like = spinner('ryan likes spinners')
 print(ryan.name)
+print(like.name)
 
 
